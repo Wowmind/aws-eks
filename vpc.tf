17d2a3e2c76ca855ad49fb11e8ea8e89d4e37ffd
wowmind <bellotaiwoa30@gmail.com>
2024-07-17 12:51:52 +0100

    updated vpc

--- vpc.tf
+++ vpc.tf
@@ -27,5 +27,25 @@ resource "aws_internet_gateway" "eks_igw"{
 }
 
 resource "aws_route_table" "eks_route_table" {
-    
+ vpc_id = aws_vpc.eks_vpc.id
+
+ route {
+    cidr_block = "0.0.0.0/0"
+    gatway_id = aws_internrt_gateway.eks_igw.id
+
+}   
+
+tags = {
+    Name = "eks-route-table"
+}
+}
+
+resource "aws_route_table_association" "eks_route_table_association" {
+    count = length(aws_subnet.eks_subnet)
+    subnet_id = element(aws_subnet.eks_subnet.*.id, count.index)
+    route_table_id = aws_route_table.eks_route_table.id
+}
+
+resource "aws_security_group" "eks_cluster_sg" {
+    vpc
 }
